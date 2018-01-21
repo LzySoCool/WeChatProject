bdeafc2cd7558dd67ee3824645ed022eddb4c7ba
LzySoCool <1012260580@qq.com>
2018-01-21 19:38:34 +0800

    任务列表
    任务列表，侧滑菜单

--- WeChat/pages/index/publishTask/taskList.js
+++ WeChat/pages/index/publishTask/taskList.js
@@ -8,7 +8,34 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    open: false,
+    mark: 0,
+    newmark: 0,
+    startmark: 0,
+    endmark: 0,
+    windowWidth: wx.getSystemInfoSync().windowWidth,
+    staus: 1,
+    translate: '',
+    taskType: [{
+        id: 1,
+        name: '美食'
+      }, {
+        id: 2,
+        name: '拼车'
+      }, {
+        id: 3,
+        name: '看电影'
+      }, {
+        id: 4,
+        name: '代购'
+      }],
+    taskItem: [{
+      title: '带份饭',
+      address: '中航国际大厦10楼'
+    }, {
+      title: '带饮料',
+      address: '赣州技师学院小红楼'
+    }]
   },
 
   /**
@@ -67,35 +94,105 @@ Page({
   
   },
 
-  // 触摸开始事件 
-  touchStart: function (e) {
-    touchDot = e.touches[0].pageX; // 获取触摸时的原点 
-    // 使用js计时器记录时间  
-    interval = setInterval(function () {
-      time++;
-    }, 100);
+  tap_ch: function (e) {
+    
   },
-  // 触摸移动事件 
-  touchMove: function (e) {
-    var touchMove = e.touches[0].pageX;
-    console.log("touchMove:" + touchMove + " touchDot:" + touchDot + " diff:" + (touchMove - touchDot));
-    // 向左滑动  
-    // if (touchMove - touchDot <= -40 && time < 10) {
-    //   wx.switchTab({
-    //     url: '../左滑页面/左滑页面'
-    //   });
-    // }
-    // 向右滑动 
-    if (touchMove - touchDot >= 40 && time < 10) {
-      console.log('向右滑动');
-      wx.switchTab({
-        url: 'task'
-      });
+  tap_start: function (e) {
+    this.data.mark = this.data.newmark = e.touches[0].pageX;
+    if(e){
+    if (this.data.staus == 1) {
+      // staus = 1指默认状态
+      this.data.startmark = e.touches[0].pageX;
+    } else {
+      // staus = 2指屏幕滑动到右边的状态
+      this.data.startmark = e.touches[0].pageX;
+    }
     }
   },
-  // 触摸结束事件 
-  touchEnd: function (e) {
-    clearInterval(interval); // 清除setInterval 
-    time = 0;
-  }, 
+  tap_drag: function (e) {
+    /*
+     * 手指从左向右移动
+     * @newmark是指移动的最新点的x轴坐标 ， @mark是指原点x轴坐标
+     */
+    if(e) {
+      if (this.data.open) {
+        this.setData({
+          translate: 'transform: translateX(10px)'
+        })
+        this.data.open = false;
+      } else {
+        this.setData({
+          translate: 'transform: translateX(' + this.data.windowWidth * 0.3 + 'px)'
+        })
+        this.data.open = true;
+      }
+    this.data.newmark = e.touches[0].pageX;
+    if (this.data.mark < this.data.newmark) {
+      if (this.data.staus == 1) {
+        if (this.data.windowWidth * 0.75 > Math.abs(this.data.newmark - this.data.startmark)) {
+          this.setData({
+            translate: 'transform: translateX(' + (this.data.newmark - this.data.startmark) + 'px)'
+          })
+        }
+      }
+
+    }
+    /*
+     * 手指从右向左移动
+     * @newmark是指移动的最新点的x轴坐标 ， @mark是指原点x轴坐标
+     */
+    if (this.data.mark > this.data.newmark) {
+      if (this.data.staus == 1 && (this.data.newmark - this.data.startmark) > 0) {
+        this.setData({
+          translate: 'transform: translateX(' + (this.data.newmark - this.data.startmark) + 'px)'
+        })
+      } else if (this.data.staus == 2 && this.data.startmark - this.data.newmark < this.data.windowWidth * 0.75) {
+        this.setData({
+          translate: 'transform: translateX(' + (this.data.newmark + this.data.windowWidth * 0.75 - this.data.startmark) + 'px)'
+        })
+      }
+    }
+  }
+    this.data.mark = this.data.newmark;
+
+  },
+  tap_end: function (e) {
+    if(e){
+      if (this.data.staus == 1 && this.data.startmark < this.data.newmark) {
+      if (Math.abs(this.data.newmark - this.data.startmark) < (this.data.windowWidth * 0.2)) {
+        this.setData({
+          translate: 'transform: translateX(0px)'
+        })
+        this.data.staus = 1;
+      } else {
+        this.setData({
+          translate: 'transform: translateX(' + this.data.windowWidth * 0.75 + 'px)'
+        })
+        this.data.staus = 2;
+      }
+    } else {
+        if (Math.abs(this.data.newmark - this.data.startmark) < (this.data.windowWidth * 0.2) && this.data.startmark != this.data.newmark) {
+        this.setData({
+          translate: 'transform: translateX(' + this.data.windowWidth * 0.75 + 'px)'
+        })
+        this.data.staus = 2;
+      } else {
+        this.setData({
+          translate: 'transform: translateX(0px)'
+        })
+        this.data.staus = 1;
+      }
+    }
+    }
+    this.data.mark = 0;
+    this.data.newmark = 0;
+  },
+  chooseTask: function(e){
+    console.log(e.currentTarget.dataset.type)
+  },
+  taskDetails: function(){
+    wx.navigateTo({
+      url: 'taskDetails',
+    })
+  }
 })
