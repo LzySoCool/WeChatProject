b04fe5781364eb138484ec2c57e466c9c80ac76b
LzySoCool <1012260580@qq.com>
2018-01-24 17:19:58 +0800

    我的
    我的页面

--- WeChat/pages/index/index.js
+++ WeChat/pages/index/index.js
@@ -8,6 +8,8 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    windowWidth: wx.getSystemInfoSync().windowWidth,
+    windowHeight: wx.getSystemInfoSync().windowHeight
   },
   //事件处理函数
   bindViewTap: function() {
@@ -22,6 +24,7 @@ Page({
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
+      console.log(app.globalData.userInfo)
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
@@ -30,6 +33,7 @@ Page({
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
+        console.log(res.userInfo)
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
@@ -39,6 +43,7 @@ Page({
       // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
         success: res => {
+          console.log(res.userInfo)
           app.globalData.userInfo = res.userInfo
           this.setData({
             userInfo: res.userInfo,
